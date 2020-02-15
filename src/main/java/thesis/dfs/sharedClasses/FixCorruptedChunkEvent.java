e873beb89839e1a033bed587bd043a225150a004
abriles <adbriles@rams.colostate.edu>
2020-02-15 15:52:43 -0700

    Made sure to actually send the message.

--- src/main/java/thesis/dfs/sharedClasses/FixCorruptedChunkEvent.java
+++ src/main/java/thesis/dfs/sharedClasses/FixCorruptedChunkEvent.java
@@ -1,6 +1,11 @@
 package thesis.dfs.sharedClasses;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+
 import thesis.dfs.messages.Message;
+import thesis.dfs.transport.TCPSender;
 
 public class FixCorruptedChunkEvent implements Runnable {
 
@@ -14,11 +19,21 @@ public class FixCorruptedChunkEvent implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		String goodChunkLocation = EventFactory.getInstance().hostToFiles.findBackupChunk(message);
+		String[] hostInformationSplit = goodChunkLocation.split(" ");
 		
 		System.out.println("Requesting this chunk server to forward a good chunk: " + goodChunkLocation);
 		
 		Message fixCorruptionMessage = new Message("FixCorruption");
 		fixCorruptionMessage.setContent(message.getContent() + goodChunkLocation);
+		
+		try {
+			TCPSender sender = new TCPSender(new Socket(hostInformationSplit[0], Integer.parseInt(hostInformationSplit[1])));
+			sender.sendData(message);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
