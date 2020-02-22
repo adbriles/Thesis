845af61b61d1b228bbfa2d310edac421865e1bd8
abriles <adbriles@rams.colostate.edu>
2020-02-22 12:04:04 -0700

    Trying to stitch files back togethor

--- src/main/java/thesis/dfs/sharedClasses/GetFileFromChunkServerEvent.java
+++ src/main/java/thesis/dfs/sharedClasses/GetFileFromChunkServerEvent.java
@@ -1,5 +1,8 @@
 package thesis.dfs.sharedClasses;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import thesis.dfs.messages.Message;
 
 public class GetFileFromChunkServerEvent implements Runnable{
@@ -14,6 +17,27 @@ public class GetFileFromChunkServerEvent implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("A chunk server sent over a chunk: " + message.getContent());
+		String chunkName = message.getContent().split(" ")[1];
+		
+		boolean isLast = EventFactory.clientGets.recordNewChunk(chunkName);
+		if(isLast) {
+			LinkedList<String> allChunks = EventFactory.clientGets.getAllChunks(chunkName);
+			stitchBackTogethor(allChunks);
+		}
+		
 	}
+	
+	
+	private void stitchBackTogethor(LinkedList<String> chunkNames) {
+		String[] inOrderChunks = new String[chunkNames.size()];
+		for(String s: chunkNames) {
+			int chunkNumber = Integer.parseInt(s.split("_chunk")[1]);
+			inOrderChunks[chunkNumber] = s;
+		}
+		for(String s: inOrderChunks) {
+			System.out.println("The chunkNumber is: " + s);
+		}
+	}
+	
 
 }
